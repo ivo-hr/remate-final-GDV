65305c03c0e12acb832785beb12c07f4bfd0b4d9
ivo-hr <enrijuan@ucm.es>
2025-09-12 16:53:41 +0200

    EjercEDA

---
+++ EDA/HelloWorld/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by enriij on 12/9/25.
+// 	EDA-GDV35 || Enrique Juan
+//
+
+#include <iostream>
+#include <ostream>
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
+}
+
